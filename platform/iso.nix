f89a9cdde81bd3450f53ddae89b650c830bfa193
SaltedFishes <main@saltedfishes.com>
2026-02-08 16:58:17 +0000

    Add a username prompt for isoImage.

--- platform/iso.nix
+++ platform/iso.nix
@@ -1,4 +1,5 @@
 {
+  conf,
   lib,
   modulesPath,
   pkgs,
@@ -15,4 +16,7 @@
   isoImage.squashfsCompression = "zstd -Xcompression-level 6";
 
   system.nixos-init.enable = lib.mkForce false;
+  services.greetd.settings.default_session.command = lib.mkForce ''
+    ${lib.getExe pkgs.tuigreet} -g "The username is '${conf.base.userName}' or 'root'."
+  '';
 }
